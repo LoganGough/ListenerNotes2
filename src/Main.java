714faed917d52998c7a9466d78d165370481cb44
logan.gough26 <logan.gough26@d155.org>
2024-01-29 11:29:50 -0600

    listeners

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+
+        JPanel panel = new JPanel();
+
+        JButton button = new JButton("Push");
+
+        JLabel label = new JLabel("");
+        JButton button2 = new JButton("Hello bob");
+
+        JTextField textField = new JTextField(12);
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("bye");
+
+            }
+        });
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                System.out.println("hi");
+                String blah = textField.getText();
+                label.setText("Hello " + blah);
+                String name = textField.getName();
+                if(name.equalsIgnoreCase("bob")){
+
+
+                }
+            }
+        });
+
+
+        ImageIcon icons = new ImageIcon("images/pony.png");
+        icons.setImage(icons.getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT));
+        JLabel picsLabel = new JLabel(icons);
+        panel.add(picsLabel);
+
+        panel.add(textField);
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+
+
+
+    }
+}
